e14802c5a2e5642d7d02f5688087b36cba2ad558
Zac <1070640731@qq.com>
2017-07-19 13:57:11 +0800

    增加归一化函数

---
+++ Python_ML_Tutorial/DecisionTree_ID3_Basic.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def create_sample():
+    features = np.array([[1.0, 0.9], [1.0, 1.0], [0.1, 0.2], [0.0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return features, labels
+
+
+def pre_processing():
+
+    return
+
+
+def core_training(training_set, training_labels):
+
+    return result_model
